71043250e95c274c1754a98c3c788fd759a4c743
padmalatha2001 <110818165+padmalatha2001@users.noreply.github.com>
2022-08-16 23:11:55 +0530

    Update HashsetUni.java

--- HashsetUni.java
+++ HashsetUni.java
@@ -4,6 +4,7 @@ public class HashsetUni {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  HashSet <Integer> h=new HashSet<Integer>();
+		//adding duplicate elements
 	       for(int i=10,j=10;i>0;i--,j--)
 	       {
 	    	   h.add(i);
